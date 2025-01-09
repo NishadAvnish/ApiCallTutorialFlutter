284d7ec8b45cb9156827087954bd2718e0b45cac
nishadavnish <avnishkumar1st@gmail.com>
2025-01-09 23:38:15 +0530

    pubspec update

--- flutter_final/lib/main.dart
+++ flutter_final/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:apicallflutter/login_page.dart';
 import 'package:apicallflutter/products.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProductListPage(),
+      home: LoginPage(),
     );
   }
 }
--- flutter_starter/lib/main.dart
+++ flutter_starter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:apicallflutter/login_page.dart';
 import 'package:apicallflutter/products.dart';
 import 'package:flutter/material.dart';
 
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProductListPage(),
+      home: LoginPage(),
     );
   }
 }
